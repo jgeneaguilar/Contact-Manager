72cf9b2178226c8fccfd7e9f99b531ed639600e0
jgeneaguilar <jgeneaguilar@gmail.com>
2019-05-18 10:43:22 +0800

    Create new file to serve as Nav Bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { 
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton 
+} from '@material-ui/core';
+import Home from '@material-ui/icons/Home';
+import PersonAdd from '@material-ui/icons/PersonAdd';
+
+// Serve as the Navigation bar and for branding
+const NavBar = ({ branding }) => {
+  return (
+    <div style={styles.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' color='inherit' style={styles.grow}>
+            {branding}
+          </Typography>
+          <IconButton color='inherit'>
+            <Home />
+          </IconButton>
+          <IconButton color='inherit'>
+            <PersonAdd />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+NavBar.defaultProps = {
+  branding: 'My App'
+}
+
+NavBar.propTypes = {
+  branding: PropTypes.string.isRequired
+}
+
+// Place the two icon buttons to the right
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  }
+};
+
+export default NavBar;
