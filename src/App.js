c7cca6dd5388b151af0d7ffbac419a04461ac19a
jgeneaguilar <jgeneaguilar@gmail.com>
2019-05-18 10:47:22 +0800

    Update Contacs.js to be the parent for both tables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from './components/Header';
+import NavBar from './components/NavBar';
 import Contacts from './components/Contacts';
+
 
 function App() {
   return (
     <div className="App">
-      <Header branding="Contact Manager" />
+      <NavBar branding="Contact Manager" />
       <Contacts />
     </div>
   );
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,5 +1,7 @@
-import React, { Component, Fragment } from 'react';
-import Contact from './Contact';
+import React, { Component } from 'react';
+import Header from './TableHeader';
+import Contact from './TableBody';
+import { Table } from '@material-ui/core';
 
 
 const mockData = [
@@ -7,19 +9,25 @@ const mockData = [
     id: 1,
     name: 'Juan Dela Cruz',
     email: 'jdcruz@gmail.com',
-    phone: '0917-000-0000'
+    phone: '0917-000-0000',
+    jobTitle: 'Unit Manager',
+    department: 'Pediatrics Ward'
   },
   {
     id: 2,
     name: 'Annie Batungbakal',
     email: 'discoqueen@gmail.com',
-    phone: '0919-000-0000'
+    phone: '0919-000-0000',
+    jobTitle: 'Staff Nurse',
+    department: 'Pediatrics Ward'
   },
   {
     id: 3,
     name: 'Maria Clara',
     email: 'ibarra\'sgirl@gmail.com',
-    phone: '0943-000-0000'
+    phone: '0943-000-0000',
+    jobTitle: 'Nursing Aide',
+    department: 'Female Ward'
   },
 ]
 
@@ -35,14 +43,15 @@ class Contacts extends Component {
     const { contacts } = this.state;
 
     return (
-      <Fragment>
+      <Table>
+        <Header />
         {contacts.map(contact => (
           <Contact 
             key={contact.id}
             contact={contact}
           />
         ))}
-      </Fragment>
+      </Table>
     );
   }
 }
