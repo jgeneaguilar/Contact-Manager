cc986d7a4bf55a6f2b0b8c8e0fd0ba506ab4c827
jgeneaguilar <jgeneaguilar@gmail.com>
2019-05-18 10:44:59 +0800

    Header component for the contacts table

---
+++ src/components/TableHeader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+const Header = () => {
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell>Name</TableCell>
+        <TableCell align='right'>Address</TableCell>
+        <TableCell align='right'>Phone</TableCell>
+        <TableCell align='right'>Job Title</TableCell>
+        <TableCell align='right'>Department</TableCell>
+        <TableCell align='right'></TableCell>
+      </TableRow> 
+    </TableHead>
+  );
+}
+
+export default Header;
